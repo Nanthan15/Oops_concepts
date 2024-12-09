952c81f80e7de989ab152717251f80df7d20399d
nanthan15 <nanthan.shetty15@gmail.com>
2024-12-09 12:46:32 +0530

    Inheritance concept commit

--- src/com/nanthan/singleton/main.java
+++
@@ -1,9 +0,0 @@
-package com.nanthan.singleton;
-
-public class main {
-    public static void main(String[] args) {
-        Singleton obj = Singleton.getInstance();
-        Singleton obj1 = Singleton.getInstance();
-        Singleton obj2 = Singleton.getInstance();
-    }
-}
